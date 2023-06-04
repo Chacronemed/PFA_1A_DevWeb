49f7fd1cf51425a87a8da82e51919446f22a22cc
mehdi1972 <mehdi_kharij@um5.ac.ma>
2023-06-04 03:00:11 +0100

    adding model-test vvariable

--- Controller/ControllerAffectation.php
+++ Controller/ControllerAffectation.php
@@ -3,54 +3,58 @@ require_once '../Model/modeleAffectattion.php';
 require_once '../Controller/ControllerEmploye.php';
 require_once '../Controller/ControllerProduits.php';
 
-class ControllerAffectation{
+class ControllerAffectation
+{
+    private $modele_test;
     private $modele;
     private $Empl;
     private $Prod;
     private $action;
     public function __construct()
-{
-    $this->modele = new modeleAffectation();
-    $this->Empl = new controlleurEmploye();
-    $this->Prod = new ControllerProduits();
-    $this->action = 'all';
-}
+    {
+        $this->modele = new modeleAffectation();
+        $this->Empl = new controlleurEmploye();
+        $this->Prod = new ControllerProduits();
+        $this->action = 'all';
+    }
 
-    public function Affichage(){
-        $this->Empl=new controlleurEmploye;
-        $this->Prod=new ControllerProduits;
-        $employes=$this->Empl->AllEmployesAction()->fetchAll();
-        $Produits=$this->Prod->ShowAllProdNonAffecterAction()->fetchAll();//AllProdNonAffecter
+    public function Affichage()
+    {
+        $this->Empl = new controlleurEmploye;
+        $this->Prod = new ControllerProduits;
+        $employes = $this->Empl->AllEmployesAction()->fetchAll();
+        $Produits = $this->Prod->ShowAllProdNonAffecterAction()->fetchAll(); //AllProdNonAffecter
         require_once '../Views/Affectation/Affectation.php';
     }
-    public function AssignAction($employe,$produit){
-        $query=$this->modele->Assign($employe,$produit);
+    public function AssignAction($employe, $produit)
+    {
+        $query = $this->modele->Assign($employe, $produit);
         return $query;
     }
-    public function action() {
-        $action = 'all'; 
-    
+    public function action()
+    {
+        $action = 'all';
+
         if (isset($_GET['action'])) {
             $action = $_GET['action'];
         } elseif (isset($_POST['action'])) {
             $action = $_POST['action'];
         }
-    
+
         switch ($action) {
             case 'all':
                 $this->Affichage();
                 break;
             case 'assign':
-                $employe=$_POST['employe_id'];
-                $produit=$_POST['produit_code'];
-                $ID=$_POST['ID_Produit'];
+                $employe = $_POST['employe_id'];
+                $produit = $_POST['produit_code'];
+                $ID = $_POST['ID_Produit'];
                 $this->Prod->AffectProduitAction($ID);
-                $this->AssignAction($employe,$produit);
+                $this->AssignAction($employe, $produit);
                 header('Location: ../ControllerAffectation.php?assigncontrol');
                 break;
         }
     }
-    
 }
-$c=new ControllerAffectation;
+$c = new ControllerAffectation;
 $c->action();
